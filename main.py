0e843a9432ac247b9ba4ed9993b989f4874e7d57
Settordici <63635493+Settordici@users.noreply.github.com>
2021-10-24 17:20:51 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import urllib3
+
+def getList(min, max):
+    http = urllib3.PoolManager()
+    arguments = "https://www.random.org/sequences/?min="+str(min)+"&max="+str(max)+"&col=1&format=plain&rnd=new"
+    r = http.request('GET', arguments)
+    out = r.data.decode()
+    out = out.split("\n")
+    for x in range(len(out) - 1):
+        out[x] = int(out[x])
+    return out
+
+def convert_list(lista):
+    lista = lista[:len(lista)-1]
+    print(lista)
+    file = open("lista.txt", "r")
+    out = file.readlines()
+    for x in lista:
+        for i in out:
+            i = i.split(".")
+            if int(i[0]) == x:
+                print(i[1], end= "")
+
+lista = getList(1, 23)
+convert_list(lista)
